7b66d65c42426fb537526b86b21f9f1662161245
boltrsss <163812338+boltrsss@users.noreply.github.com>
2026-08-27 20:08:55 +0800

    Add files via upload

--- src/components/ReviewCards.tsx
+++ src/components/ReviewCards.tsx
@@ -24,9 +24,9 @@ export default function ReviewCards() {
       initial: 'PM',
       name: 'Presley McKnight',
       verified: true,
-      title: 'Purely white is SUPERIOR',
+      title: 'Gleam Mint is SUPERIOR',
       content: [
-        "No sensitivity, and instant white results! I love love purely white!!!!!"
+        "No sensitivity, and instant white results! I love love GleamMint whitening!!!!!"
       ]
     },
     {
